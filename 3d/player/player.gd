7ecdaae121d3718bfb27c97f06168f42ce3282ec
khar20 <136043524+khar20@users.noreply.github.com>
2025-09-19 22:37:05 -0500

    Refactor main scene scripts and update player controls; add fire effect to 3D scene

--- 3d/player/player.gd
+++ 3d/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const SENS = 0.005
@@ -20,9 +19,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
