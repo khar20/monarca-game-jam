689b5975bf7cbb1c49b6d4933d83a736a81c622e
AlexQuintana147 <alexquintana12622@hotmail.com>
2025-09-20 00:13:36 -0500

    menu opcion rota v2

--- 2D/Escenas2D/menu_2d.gd
+++ 2D/Escenas2D/menu_2d.gd
@@ -1,14 +1,21 @@
 extends Control
 
-
+@onready var configuracion_button = $VBoxContainer/Configuracion
+@onready var salir_button = $VBoxContainer/Salir
+@onready var configuracion_rota = $VBoxContainer/ConfiguracionRota
+@onready var salir_roto = $VBoxContainer/SalirRoto
 
 func _on_jugar_pressed() -> void:
 	get_tree().change_scene_to_file("res://2D/Escenas2D/game_2d.tscn")
 
-
 func _on_configuracion_pressed() -> void:
-	pass # Replace with function body.
-
+	# Ocultar el botón y mostrar la imagen rota
+	configuracion_button.visible = false
+	configuracion_rota.visible = true
+	
 
 func _on_salir_pressed() -> void:
-	get_tree().quit()
+	# Ocultar el botón y mostrar la imagen rota
+	salir_button.visible = false
+	salir_roto.visible = true
+	
