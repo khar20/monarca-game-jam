95a63116374094ac70c69c8d53feefdb5c6b79b7
AlexQuintana147 <alexquintana12622@hotmail.com>
2025-09-22 02:51:12 -0500

    SaltarNoLiveCamara

--- 2d/main_scene/main_scene.gd
+++ 2d/main_scene/main_scene.gd
@@ -3,7 +3,3 @@ extends Node2D
 @onready var player = $Player2D
 @onready var camera = $Camera2D
 @onready var crt_shader = $CanvasLayer/TextureRect
-
-func _process(_delta: float) -> void:
-	# camera.position.x = player.position.x  # Comentado para mantener la cámara fija
-	crt_shader.position = camera.position
