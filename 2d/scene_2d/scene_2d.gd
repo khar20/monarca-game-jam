be3dfaeadb8e00c52a2937cdf548d6253bf9372f
khar20 <136043524+khar20@users.noreply.github.com>
2025-09-26 17:59:32 -0500

    feat: Add end scene functionality and implement crash effects

--- 2d/scene_2d/scene_2d.gd
+++ 2d/scene_2d/scene_2d.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 signal scene_change_requested(scene_path: String)
+signal coin_collected
 
 @onready var player_2d: CharacterBody2D = $Player2D
 @onready var menu_final_area: Area2D = $MenuFinal
 #@onready var crt_shader: ColorRect = $CanvasLayer/TextureRect
 @onready var count_label: Label = $Control/Label
+
 var coin_count: int = 0
 
 func _ready() -> void:
@@ -13,6 +15,8 @@ func _ready() -> void:
 	# Conectar la señal de colisión del MenuFinal
 	if menu_final_area:
 		menu_final_area.body_entered.connect(_on_menu_final_body_entered)
+	
+	coin_collected.connect(_on_coin_coin_collected)
 
 func update_display() -> void:
 	count_label.text = "x " + str(coin_count)
@@ -31,4 +35,4 @@ func _on_menu_final_body_entered(body: Node2D) -> void:
 	# Verificar si el cuerpo que entró es el jugador
 	if body == player_2d:
 		# Cambiar a la escena del menu final
-		scene_change_requested.emit("res://2d/main_menu_2d/menu_final.tscn")
+		scene_change_requested.emit("res://2d/end_scene_2d/menu_final.tscn")
